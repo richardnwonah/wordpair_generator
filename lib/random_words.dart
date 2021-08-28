52fcf14f44479b88e247fb905ee162ccfb5e1f49
Richard nwonah <nwonahr@gmail.com>
2021-08-28 12:24:22 +0100

    complicted app by adding favourite word screen page

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -42,8 +42,31 @@ class RandomWordsState extends State<RandomWords> {
         });
   }
 
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
+
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+          appBar: AppBar(title: Text('Saved WordPairs')),
+          body: ListView(children: divided));
+    }));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('wordPair Generator')), body: _buildList());
+        appBar: AppBar(
+          title: Text('wordPair Generator'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+          ],
+        ),
+        body: _buildList());
   }
 }
